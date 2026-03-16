7a5299c3bc3669ed1faf849c1c45be907617ef91
ozzienobayrak <albayrak.ozlem@gmail.com>
2026-03-16 17:19:17 +0100

    QRcodecorrected

--- app/streamlit.py
+++ app/streamlit.py
@@ -63,7 +63,7 @@ def generate_qr_code(url):
     buf.seek(0)
     return buf
 
-APP_URL = "https://your-app-url.streamlit.app"
+APP_URL = "https://ozzienobayrak-retail-price-revenue-optimisa-appstreamlit-0xfpzl.streamlit.app/"
 
 def prettify_table(df: pd.DataFrame) -> pd.DataFrame:
     return df.rename(columns=DISPLAY_RENAME)
